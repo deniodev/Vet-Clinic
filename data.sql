69997963254926608bd89f960bcbfce1feea06e9
mohametalmeari <mohamet.almeari@gmail.com>
2023-07-17 15:20:57 +0300

    Create email column & insert data

--- data.sql
+++ data.sql
@@ -76,3 +76,14 @@ VALUES((SELECT id FROM animals WHERE name = 'Agumon'), (SELECT id FROM vets WHER
 ((SELECT id FROM animals WHERE name = 'Boarmon'), (SELECT id FROM vets WHERE name = 'Maisy Smith'), 'Aug 3, 2020'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), 'May 24, 2020'),
 ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), 'Jan 11, 2021');
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) 
+SELECT * FROM (SELECT id FROM animals) animal_ids, 
+(SELECT id FROM vets) vets_ids, 
+generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+
+insert into owners (full_name, email) 
+select 'Owner ' || generate_series(1,2500000), 
+'owner_' || generate_series(1,2500000) || '@mail.com';
